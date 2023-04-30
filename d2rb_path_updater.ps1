9ede0f2a0b463e053e6913a9e92baf2683f4e398
Thrackx <127045239+Thrackx@users.noreply.github.com>
2023-04-29 23:55:22 -0400

    Update d2rb_path_updater.ps1

--- d2rb_path_updater.ps1
+++ d2rb_path_updater.ps1
@@ -1,16 +1,16 @@
-# D2RB Path Updater
+# D2RB Updater
 # Written by Thrack
 
 
 # Instructions:
-# Update the two variables below based on where your previous settings file is and where the new bot folder is.
+# Update the two variables below based on where your previous bot folder is and where the new bot folder is.
 # Once set, save the script and run it in Powershell ISE as an administrator.
 
-# Define the old path of the settings.json file.  Example:  C:\D2RB3.2585\Day\settings.json
-$oldSettingsJsonPath = "INSERT PATH OF SETTINGS.JSON"
+# Define the old path of the bot.  This is the bot folder where Day and D2RB reside.  Ensure the path ends with a "\".  Example:  C:\D2RB3.2582\
+$oldSettingsBotPath = "INSERT OLD BOT LOCATION"
 
 # Define the folder for the new bot.  Ensure the path ends with a "\".  Example: C:\D2RB3.2585\
-$newSettingsBotPath = "INSERT PATH OF NEW BOT FOLDER"
+$newSettingsBotPath = "INSERT OLD BOT LOCATION"
 
 
 
@@ -18,6 +18,7 @@ $newSettingsBotPath = "INSERT PATH OF NEW BOT FOLDER"
 
 # Variables
 $newSettingsJsonPath = Join-Path -Path $newSettingsBotPath -ChildPath "\Day"
+$oldSettingsJsonPath = Join-Path -Path "$oldSettingsBotPath" -ChildPath "\Day\settings.json"
 
 # Check if settings.json exists in the old path location
 if (Test-Path -Path $oldSettingsJsonPath) {
@@ -33,8 +34,8 @@ if (Test-Path -Path $oldSettingsJsonPath) {
 
     # Replace the BotPath in settings.json with the new destination path
     $content = Get-Content $newsettingsfilepath -Raw
-    $content = $content -replace '("BotPath":\s*")[^"]+(")', "$1$($newSettingsBotPath -replace '\\', '\\')D2RB\\D2RB.exe$2"
-    Set-Content $newsettingsfilepath -Value $content 
+    $content = $content -replace '(\"BotPath\":\s*\")[^"]+(\"),', "`$1$($newSettingsBotPath -replace '\\', '\\')D2RB\\D2RB.exe`$2,"
+    Set-Content $newsettingsfilepath -Value $content  
 
     Write-Host "The settings.json file was copied to the new location and updated successfully"
 } else {
